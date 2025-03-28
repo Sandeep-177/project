2d54ab9f4bdcbf859f88b6e97ac7fd86525bee97
Sandeep-177 <sandeepkumarpradhan177@gmail.com>
2025-03-28 07:49:20 +0530

    console added for svg

--- customer_html/headFootCm.js
+++ customer_html/headFootCm.js
@@ -76,16 +76,20 @@
 
         document.addEventListener("DOMContentLoaded", function () {
             const images = document.querySelectorAll(".svgLnk");
-            console.log("svgLnk found");
+            console.log(`svgLnk elements found: ${images.length}`);
             images.forEach(img => {
+                console.log("processing:",img);
                 let svgTest= window.location.hostname.includes("github.io");
+                let currentSrc=img.getAttribute("src");
+                console.log(`current src: ${currentSrc}`);
                 if (svgTest) {
                     img.setAttribute("src", "/project" + img.getAttribute("src"));
+                    console.log(`Updated src: ${img.getAttribute("src")}`);
                 }else{
-                    console.log("svgLnk not found, line 85")
+                    console.log("svgLnk not found, line 89")
                 }
             });
-            console.log("svgs loaded, line 88")
+            console.log("svgs loaded, line 92");
         });
         
 
